9516ac54bfd759468e8e8af65b2aa68c75aa88a5
RainbowJM <Jevona98@live.com>
2021-11-02 13:04:17 +0100

    main done

---
+++ src/backend/main/java/nl/hu/consultant/SkilltreeConsultantApplication.java
@@ -0,0 +1,13 @@
+package nl.hu.consultant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkilltreeConsultantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkilltreeConsultantApplication.class, args);
+    }
+
+}
