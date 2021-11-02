d415c4c71eecf4591956a17e6c584e3e9f81eeda
RainbowJM <Jevona98@live.com>
2021-11-02 13:09:37 +0100

    added structure test and resources

---
+++ src/backend/main/test/java/nl/hu/consultant/SkilltreeConsultantApplicationTest.java
@@ -0,0 +1,15 @@
+package nl.hu.consultant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class SkilltreeConsultantApplicationTest {
+
+    @Test
+    void contextLoads(){
+
+    }
+}
