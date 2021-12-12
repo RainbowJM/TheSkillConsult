78ba9b0db4476f6e1781588d5b5a13ce0ce87c92
RainbowJM <Jevona98@live.com>
2021-12-12 18:56:15 +0100

    from class to interface

--- src/backend/main/java/nl/hu/consultant/security/persistence/UserRepository.java
+++ src/backend/main/java/nl/hu/consultant/security/persistence/UserRepository.java
@@ -5,16 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public class UserRepository {
-    /**
-     * This is a magic interface, which is converted
-     * to a class during compilation.
-     * <p>
-     * Note that this introduces coupling between Chips and the way they are stored!
-     * In more loosely coupled components, you would add an intermediary abstraction
-     * like an abstract repository or a DAO!
-     */
-    public interface SpringUserRepository extends JpaRepository<User, Long> {
-        Optional<User> findByUsername(String username);
-    }
+/**
+ * This is a magic interface, which is converted
+ * to a class during compilation.
+ * <p>
+ * Note that this introduces coupling between Chips and the way they are stored!
+ * In more loosely coupled components, you would add an intermediary abstraction
+ * like an abstract repository or a DAO!
+ */
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
+
