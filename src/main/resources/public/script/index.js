2da0bace761e1b49ffb335bebf892972cd5dfefd
RainbowJM <Jevona98@live.com>
2022-01-28 00:47:44 +0100

    with good logging it redirects you to the correct page

--- src/main/resources/public/script/index.js
+++ src/main/resources/public/script/index.js
@@ -2,24 +2,41 @@ const LOGINBUTTON = document.getElementById("sign-in-btn");
 
 LOGINBUTTON.addEventListener("click", signin);
 
-function signin(){
+function signin() {
     let unameElement = document.getElementById("username").value;
     let pswElement = document.getElementById("password").value;
     console.log("elementValue", unameElement, pswElement);
-    if(unameElement.trim() === "" || pswElement.trim() === ""){
+    if (unameElement.trim() === "" || pswElement.trim() === "") {
         alert("Please enter username and password");
         return false;
-    }else{
+    } else {
         let payload = {
             "username": unameElement,
             "password": pswElement
         }
         fetch('/login', {method: 'POST', body: JSON.stringify(payload)})
             .then(function (response) {
-                if(response.ok) {
-                    console.log("h");
-                    window.location = "Studentoverzicht.html";
-                }else {
+                if (response.ok) {
+                    console.log(response.headers.get('Authorization'))
+                    let token = response.headers.get('Authorization');
+                    let subToken = token.substr(6,token.length)
+                    console.log(subToken)
+                    let convertPayload = parseJwt(subToken);
+                    let role = convertPayload.ROLE
+
+                    if (role === "TEACHER"){
+                        console.log(" teacher window")
+                        window.location = "DocentStudentoverzicht.html"
+                    }else{
+                        console.log("student window")
+                        window.location = "Studentoverzicht.html"
+                    }
+                    //     console.log(payload);
+                    // console.log(payload.ROLE);
+                    // checkRole();
+                    // window.location = "Studentoverzicht.html";
+                    // checkRole();
+                } else {
                     console.log("HH")
                     alert("Wrong username/password")
                     throw "Wrong username/password";
@@ -27,5 +44,41 @@ function signin(){
             })
             .catch(error => console.log(error));
         return true;
+    }
+}
+
+function parseJwt (token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
+}
+
+function checkRole() {
+    console.log("WE gonna check the role")
+    let unameElement = document.getElementById("username").value;
+    console.log("elementValue", unameElement);
+
+    if (unameElement.trim() === "") {
+        return false;
+    } else {
+        let payload = {
+            "username": unameElement,
+        }
+        fetch('/user/role/admin', {method: 'GET'})
+            .then(function (response){
+                if (response.ok){
+                    console.log(response.json());
+                    return response.json();
+                }else{
+                    console.log("wrong")
+                    throw "ERROR"
+                }
+            })
+            .catch(error => console.log(error));
+            return true;
     }
 }
